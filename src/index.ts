2fef210f0a1129e631c25e4ebd6929fa23455994
Alvin Chan <aschan@gmail.com>
2017-05-04 01:52:46 -0700

    Add Knex database adapter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 // Dependencies
 import {microGraphql} from 'graphql-server-micro'
+import * as knex from 'knex'
+
+import databaseConfig from './config/database'
 import schema from './schema'
 
-module.exports = microGraphql({schema})
+// Context
+const context = {
+  db: knex(databaseConfig)
+}
+
+module.exports = microGraphql({schema, context})
